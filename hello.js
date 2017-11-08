f99e91aacdc571b152514675d73871fc3e9a35b4
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-11-08 17:16:29 -0500

    1st lesson in expressworks

--- hello.js
+++ hello.js
@@ -1,9 +1,9 @@
 var express = require('express')
 var app = express()
-app.get('/', function(req, res) {
+app.get('/home', function(req, res) {
   res.end('Hello World!')
 })
-app.listen(3000)
+app.listen(process.argv[2] || 3000)
 
 
 /*
