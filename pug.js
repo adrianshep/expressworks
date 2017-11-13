052d845d3e978735ab94eda531e2c09b033691e8
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-11-13 09:31:18 -0500

    3rd lesson in expressworks

--- pug.js
+++ pug.js
@@ -1,7 +1,7 @@
 var express = require('express')
 var app = express()
 app.set('view engine', 'jade')
-app.set('views' process.argv[3])
+app.set('views', process.argv[3])
 app.get('/home', function(req, res) {
   res.render('index', {date: new Date().toDateString()})
 })
