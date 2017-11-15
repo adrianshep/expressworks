b1751f32845bc7b1c39e875d0a33a74cc8738a00
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-11-15 09:24:34 -0500

    4th lesson in expressworks

--- form.js
+++ form.js
@@ -1,4 +1,14 @@
+var express = require('express')
+var bodyParser = require('body-parser')
+var app = express()
 
+app.use(bodyParser.urlencoded({extended: false}))
+
+app.post('/form', function(req, res) {
+  res.send(req.body.str.spit('').reverse().join(''))
+})
+
+app.listen(process.argv[2] || 3000)
 
 
 /*
