bf48c8d9937ec8b4224f3cd2f3b2ea6f7edadfe5
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-11-17 09:23:30 -0500

    4th lesson in expressworks

--- form.js
+++ form.js
@@ -13,6 +13,25 @@ app.listen(process.argv[2] || 3000)
 
 /*
 
+
+Here's the official solution in case you want to compare notes:
+
+────────────────────────────────────────────────────────────────────────────────
+    var express = require('express')
+    var bodyParser = require('body-parser')
+    var app = express()
+
+    app.use(bodyParser.urlencoded({extended: false}))
+
+    app.post('/form', function(req, res) {
+      res.send(req.body.str.split('').reverse().join(''))
+    })
+
+    app.listen(process.argv[2])
+
+────────────────────────────────────────────────────────────────────────────────
+
+
 GOOD OLD FORM
  Exercise 4 of 8
 
