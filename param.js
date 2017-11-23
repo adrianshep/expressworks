2581c985630c12251d6bf389fa52042b439c2c3a
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-11-23 09:21:59 -0500

    6th lesson in expressworks

--- param.js
+++ param.js
@@ -14,6 +14,26 @@ app.listen(process.argv[2] || 3000)
 
 /*
 
+Here's the official solution in case you want to compare notes:
+
+────────────────────────────────────────────────────────────────────────────────
+    var express = require('express')
+    var app = express()
+
+    app.put('/message/:id', function(req, res){
+      var id = req.params.id
+      var str = require('crypto')
+        .createHash('sha1')
+        .update(new Date().toDateString() + id)
+        .digest('hex')
+      res.send(str)
+    })
+
+    app.listen(process.argv[2])
+
+────────────────────────────────────────────────────────────────────────────────
+
+
 PARAM PAM PAM
  Exercise 6 of 8
 
