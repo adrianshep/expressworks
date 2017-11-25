9b6b5f150662e81b0222a1bbe95895b836414614
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-11-25 08:57:06 -0500

    7th lesson in expressworks

--- query.js
+++ query.js
@@ -1,4 +1,12 @@
+var express = require('express')
+var app = express()
 
+app.get('/search', function(req, res){
+  var query = req.query
+  res.send(query)
+})
+
+app.listen(process.argv[2] || 3000)
 
 
 /*
