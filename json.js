e36c991ca03443fb81347bc56fbbe77eb0a15dd6
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-11-26 09:47:56 -0500

    8th lesson in expressworks

---
+++ json.js
@@ -0,0 +1,46 @@
+
+
+
+/*
+
+JSON ME
+ Exercise 8 of 8
+
+Most of the times we're building RESTful API servers with JSON.
+
+Write a server that, when it receives a GET, reads a file, parses it to JSON,
+and responds with that content to the user.
+
+The server should respond to any GET that matches the /books resource path.
+As always, the port is passed in process.argv[2]. The file to read is passed
+in process.argv[3].
+
+Respond with:
+
+    res.json(object)
+
+Everything should match the /books resource path.
+
+For reading the file, use the fs module, e.g.,
+
+    fs.readFile(filename, callback)
+
+-------------------------------------------------------------------------------
+
+## HINTS
+
+While the parsing can be done with JSON.parse:
+
+    object = JSON.parse(string)
+
+No need to install the fs module. It's part of the core and the Node.js platform.
+
+Videos: [http://bit.ly/1jW1sBf](http://bit.ly/1jW1sBf).
+
+
+ » To print these instructions again, run: expressworks print
+ » To execute your program in a test environment, run: expressworks run program.js
+ » To verify your program, run: expressworks verify program.js
+ » For help run: expressworks help
+
+*/
