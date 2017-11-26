2b61915ec8151fee8aa3f48ec57890a4af59aa23
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-11-26 09:46:16 -0500

    7th lesson in expressworks

--- query.js
+++ query.js
@@ -11,6 +11,21 @@ app.listen(process.argv[2] || 3000)
 
 /*
 
+Here's the official solution in case you want to compare notes:
+
+────────────────────────────────────────────────────────────────────────────────
+    var express = require('express')
+    var app = express()
+
+    app.get('/search', function(req, res){
+      var query = req.query
+      res.send(query)
+    })
+
+    app.listen(process.argv[2])
+
+────────────────────────────────────────────────────────────────────────────────
+
 WHAT'S IN QUERY
 Exercise 7 of 8
 
