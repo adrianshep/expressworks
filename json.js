42cc5f7a6916fada71ad9ec3f4d733310241553f
Adrian Shepelavey <adrianshepelavey@gmail.com>
2017-11-28 09:39:37 -0500

    8th lesson in expressworks

--- json.js
+++ json.js
@@ -21,6 +21,30 @@ app.listen(process.argv[2])
 
 /*
 
+Here's the official solution in case you want to compare notes:
+
+────────────────────────────────────────────────────────────────────────────────
+    var express = require('express')
+    var app = express()
+    var fs = require('fs')
+
+    app.get('/books', function(req, res){
+      var filename = process.argv[3]
+      fs.readFile(filename, function(e, data) {
+        if (e) return res.sendStatus(500)
+        try {
+          books = JSON.parse(data)
+        } catch (e) {
+          res.sendStatus(500)
+        }
+        res.json(books)
+      })
+    })
+
+    app.listen(process.argv[2])
+
+────────────────────────────────────────────────────────────────────────────────
+
 JSON ME
  Exercise 8 of 8
 
